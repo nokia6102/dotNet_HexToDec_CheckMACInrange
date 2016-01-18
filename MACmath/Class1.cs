dbe0c38e4ccbbf021eda52a40b2fa597bf941a2b
max <nokia6102@gmail.com>
2016-01-18 14:39:40 +0800

    First Commit , dotNet Hex2dec and checkInRage class

---
+++ MACmath/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MACmathS
+{
+    public class MACmath
+    {
+        public bool InRnage(String ss,String ee,String ValueV)
+        { 
+          Int64 s,e,Value;
+          s=  Int64.Parse(ss, System.Globalization.NumberStyles.HexNumber);
+          e=  Int64.Parse(ee, System.Globalization.NumberStyles.HexNumber);
+          Value = Int64.Parse(ValueV, System.Globalization.NumberStyles.HexNumber);
+          return (Value >= s && Value <= e);
+        }
+    }
+}
---
+++ testMACmath/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MACmathS;
+
+namespace testMACmath
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MACmath ck = new MACmath();
+            if (ck.InRnage(textBox1.Text, textBox2.Text, textBox3.Text))
+            {
+                label1.Text = "In Rnage";
+            }
+            else
+            {
+                label1.Text = "Not In Rnage";
+            }
+        }
+    }
+}
